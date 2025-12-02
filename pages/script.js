6e3e67337a7684591ddf71be758f059df9f3ff2e
L-L-E-N-A-L <lena.fougeron@etu.univ-amu.fr>
2025-12-02 15:23:43 +0100

    ajout d'un bouton pour switch de theme et un autre pour remonter en haut de la page + ajout du deuxième style css

--- pages/script.js
+++ pages/script.js
@@ -15,4 +15,52 @@ menu.addEventListener("click", (event) => {
 });
 
 
+ function toggleTheme() {
 
+  // Select the <link> element
+  let theme = document.getElementById('theme');
+  // Toggle between light.css and dark.css
+
+  if (theme.getAttribute('href') == 'light_mod.css') {
+  theme.setAttribute('href', 'dark_mod.css');
+
+  } else {
+  theme.setAttribute('href', 'light_mod.css');
+  }
+
+}
+//for games pages, since the path to the css file isn't the same
+function toggleThemeG() {
+
+  // Select the <link> element
+  let theme = document.getElementById('themeG');
+  // Toggle between light.css and dark.css
+
+  if (theme.getAttribute('href') == '../light_mod.css') {
+  theme.setAttribute('href', '../dark_mod.css');
+
+  } else {
+  theme.setAttribute('href', '../light_mod.css');
+  }
+
+}
+
+// Get the button:
+let mybutton = document.getElementById("topButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
